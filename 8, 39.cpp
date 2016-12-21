31e771a851bcd206daf229e47cb2dd8e0099df2c
Voldemar <shlykov.vov@yandex.ru>
2016-12-21 22:32:58 +0300

    Add files via upload

---
+++ 8, 39.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+
+int main()
+{
+
+	int const N = 4, M = 4;
+
+	int matrix[N][M];
+
+
+
+	for (int i = 0; i < N; i++)
+
+
+
+	{
+
+
+
+		for (int j = 0; j < M; j++)
+
+
+
+		{
+
+
+
+			cout << "[" << (i + 1) << "][" << (j + 1) << "] = ";
+
+
+
+			cin >> matrix[i][j];
+
+
+
+		}
+
+
+
+	}
+
+
+
+	{
+
+		int j = 0;
+
+		while (j < k) {
+
+			for (int i = 0; i < n - 1; i++)
+
+				Swap(a[i], a[i + 1]);
+
+			j++;
+
+		}
+
+
+
+
+
+	for (int i = 0; i < N; i++)
+
+
+
+	{
+
+
+
+		for (int j = 0; j < M; j++)
+
+
+
+		{
+
+
+
+			cout.width(5);
+
+
+
+			cout << matrix[i][j];
+
+
+
+		}
+
+
+
+		cout << endl;
+
+
+
+	}
+
+}
+
+
